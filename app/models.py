fd6d016884f66d7dd8c8d3a866bd16129d4d5630
Jesus Jimenez <jjimenechuyin@gmail.com>
2025-03-17 13:45:12 -0600

    Update models.py
    no more models

--- app/models.py
+++ app/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from datetime import datetime
-from django.utils import timezone
-
 
 from db_conn import db
 
 users_collection = db['Users']
 sensores = db['Registro']
 
-class Lectura_sen(models.Model):
-    pulsaciones = models.IntegerField()
-    oxigenacion = models.FloatField()
-    temperatura = models.FloatField()
-    fecha_hora = models.DateTimeField(default=timezone.now)
 
 
