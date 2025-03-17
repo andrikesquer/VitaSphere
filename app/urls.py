53cfbf146643203b7451af1dc6c9e7f9a8171eca
Jesus Jimenez <jjimenechuyin@gmail.com>
2025-03-17 13:44:06 -0600

    Update urls.py
    .

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,9 @@ from rest_framework import routers
 
 from app import views
 
-router=routers.DefaultRouter()
-router.register(r'sensores',views.SensoresViewSet)
 urlpatterns=[
     path('metrica/', views.metrica),
-    path('ver/',include(router.urls))
+   
 ]
 
 
